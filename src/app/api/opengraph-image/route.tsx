53ab2433207c1924be2deff1ef5637aab6ca4111
Deepak973 <deepak.rj.dr@gmail.com>
2025-09-10 00:12:58 +0530

    test-og

--- src/app/api/opengraph-image/route.tsx
+++ src/app/api/opengraph-image/route.tsx
@@ -9,13 +9,13 @@ export async function GET(request: NextRequest) {
       <div tw="flex h-full w-full flex-col justify-center items-center relative bg-primary">
         <div tw="flex w-96 h-96 rounded-full overflow-hidden mb-8 border-8 border-white">
           <img
-            src={"https://aave-earn.vercel.app/splash.png"}
-            alt="EOA"
+            src={"https://farcastersnake.vercel.app/logo.png"}
+            alt="Farcaster Snake"
             tw="w-full h-full object-cover"
           />
         </div>
 
-        <h1 tw="text-white text-center text-4xl font-bold">EOA</h1>
+        <h1 tw="text-8xl text-purple-500">Eat, Drink, Poop Repeat</h1>
       </div>
     ),
     {
