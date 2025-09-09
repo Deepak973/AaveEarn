6b1529ecf39c9a3383de46e1bbe3ea6f3b1ae25c
Deepak973 <deepak.rj.dr@gmail.com>
2025-09-09 23:48:41 +0530

    fix: fixed og-iamge issue

--- src/app/api/opengraph-image/route.tsx
+++ src/app/api/opengraph-image/route.tsx
@@ -4,17 +4,10 @@ import { NextRequest } from "next/server";
 export const dynamic = "force-dynamic";
 
 export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
-  const fid = searchParams.get("fid");
-
   return new ImageResponse(
     (
       <div tw="flex h-full w-full flex-col justify-center items-center relative bg-primary">
-        <img
-          src="https://aave-earn.vercel.app/splash.png"
-          alt="Profile"
-          tw="w-full h-full object-cover"
-        />
+        <img src="/splash.png" alt="Profile" tw="w-full h-full object-cover" />
         <h1 tw="text-white text-center text-4xl font-bold">EOA</h1>
       </div>
     ),
