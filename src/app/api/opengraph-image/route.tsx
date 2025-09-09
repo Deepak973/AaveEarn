f15a3eb432c0e91ec5a796b79a34b6dcabdb383d
Deepak973 <deepak.rj.dr@gmail.com>
2025-09-10 00:02:26 +0530

    fix: og-image issue

--- src/app/api/opengraph-image/route.tsx
+++ src/app/api/opengraph-image/route.tsx
@@ -7,7 +7,11 @@ export async function GET(request: NextRequest) {
   return new ImageResponse(
     (
       <div tw="flex h-full w-full flex-col justify-center items-center relative bg-primary">
-        <img src="/splash.png" alt="Profile" tw="w-full h-full object-cover" />
+        <img
+          src={"https://aave-earn.vercel.app/splash.png"}
+          alt="Profile"
+          tw="w-full h-full object-cover"
+        />
         <h1 tw="text-white text-center text-4xl font-bold">EOA</h1>
       </div>
     ),
