e1c173c2f7f85d79bacfb450b2ac27af1b4c7e85
Deepak973 <deepak.rj.dr@gmail.com>
2025-09-10 00:05:43 +0530

    fix: trying og-img sol

--- src/app/api/opengraph-image/route.tsx
+++ src/app/api/opengraph-image/route.tsx
@@ -7,11 +7,14 @@ export async function GET(request: NextRequest) {
   return new ImageResponse(
     (
       <div tw="flex h-full w-full flex-col justify-center items-center relative bg-primary">
-        <img
-          src={"https://aave-earn.vercel.app/splash.png"}
-          alt="Profile"
-          tw="w-full h-full object-cover"
-        />
+        <div tw="flex w-96 h-96 rounded-full overflow-hidden mb-8 border-8 border-white">
+          <img
+            src={"https://aave-earn.vercel.app/splash.png"}
+            alt="EOA"
+            tw="w-full h-full object-cover"
+          />
+        </div>
+
         <h1 tw="text-white text-center text-4xl font-bold">EOA</h1>
       </div>
     ),
